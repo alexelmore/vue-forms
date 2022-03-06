33207bb1aaa9b70fc0644d8ca6286df55375bb6b
aelmore49 <alexelmore49@gmail.com>
2022-03-06 09:10:02 -0600

    Finish Vue form practice project

--- vue-basic-forms/src/components/SignupForm.vue
+++ vue-basic-forms/src/components/SignupForm.vue
@@ -1,18 +1,30 @@
 <template>
-	<form>
+	<form @submit.prevent="submitForm">
 		<label>Email</label>
 		<input type="email" required v-model="email" />
 		<label>Password</label>
 		<input type="password" required v-model="password" />
+		<div v-if="passwordError" class="error">
+			{{ passwordError }}
+		</div>
 		<label>Role</label>
 		<select v-model="role">
 			<option value="developer">Web Developer</option>
 			<option value="desinger">Web Designer</option>
 		</select>
 
+		<label>Skills:</label>
+		<input type="text" v-model="tempSkill" @keyup="addSkill" />
+		<div v-for="skill in skills" :key="skill" class="pill" @click="deleteSkill">
+			{{ skill }}
+		</div>
+
 		<div class="terms">
 			<input type="checkbox" required v-model="terms" />
 			<label>Accept terms and conditions</label>
+		</div>
+		<div class="submit">
+			<button>Create An Account</button>
 		</div>
 	</form>
 	<div>
@@ -20,7 +32,6 @@
 		<p>Password:{{ password }}</p>
 		<p>Role:{{ role }}</p>
 		<p>Terms accepted:{{ terms }}</p>
-		{{ names }}
 	</div>
 </template>
 
@@ -33,7 +44,46 @@ export default {
 			password: "",
 			role: "",
 			terms: false,
+			tempSkill: "",
+			skills: [],
+			passwordError: "",
 		};
+	},
+	methods: {
+		addSkill(e) {
+			if (e.key === "," && this.tempSkill) {
+				if (!this.skills.includes(this.tempSkill)) {
+					this.skills.push(this.tempSkill.slice(0, -1));
+				}
+				this.tempSkill = "";
+			}
+		},
+		deleteSkill(e) {
+			let skill = e.target.innerHTML;
+			let filteredArray = this.skills.filter((s) => s !== skill);
+			this.skills = [...filteredArray];
+		},
+		submitForm() {
+			this.passwordError =
+				this.password.length >= 5
+					? ""
+					: "Password must be at least 5 characters long";
+
+			if (!this.passwordError) {
+				// make request to database to save user
+				console.log("email: ", this.email);
+				console.log("password: ", this.password);
+				console.log("role: ", this.role);
+				console.log("skills: ", this.skills);
+				console.log("terms accepted: ", this.terms);
+				// Reset form fields
+				this.email = "";
+				this.password = "";
+				this.role = "";
+				this.skills = [];
+				this.terms = false;
+			}
+		},
 	},
 };
 </script>
@@ -72,5 +122,34 @@ input[type="checkbox"] {
 	margin: 0 10px 0 0;
 	position: relative;
 	top: 2px;
+}
+.pill {
+	display: inline-block;
+	margin: 20px 10px 0 0;
+	padding: 6px 12px;
+	background: #eee;
+	border-radius: 20px;
+	font-size: 12px;
+	letter-spacing: 1px;
+	font-weight: bold;
+	color: #777;
+	cursor: pointer;
+}
+button {
+	background: #0b6dff;
+	border: 0;
+	padding: 10px 20px;
+	margin-top: 20px;
+	color: white;
+	border-radius: 20px;
+}
+.submit {
+	text-align: center;
+}
+.error {
+	color: #ff0062;
+	margin-top: 10px;
+	font-size: 0.8em;
+	font-weight: bold;
 }
 </style>
